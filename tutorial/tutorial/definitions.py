c538f1bc32ab87c723aa7102e49d88d04f9f5dbf
lyamlim97 <lyamlim97@gmail.com>
2024-12-07 23:05:03 +0800

    First dagster project

---
+++ tutorial/tutorial/definitions.py
@@ -0,0 +1,24 @@
+from dagster import AssetSelection, Definitions, ScheduleDefinition, load_assets_from_modules, EnvVar
+
+from . import assets
+from .resources import DataGeneratorResource
+
+all_assets = load_assets_from_modules([assets])
+
+# Addition: a ScheduleDefinition targeting all assets and a cron schedule of how frequently to run it
+hackernews_schedule = ScheduleDefinition(
+    name="hackernews_schedule",
+    target=AssetSelection.all(),
+    cron_schedule="0 * * * *",  # every hour
+)
+
+datagen = DataGeneratorResource(
+    num_days=EnvVar.int("HACKERNEWS_NUM_DAYS_WINDOW"),
+)
+defs = Definitions(
+    assets=all_assets,
+    schedules=[hackernews_schedule],
+    resources={
+        "hackernews_api": datagen,  # Add the newly-made resource here
+    },
+)
